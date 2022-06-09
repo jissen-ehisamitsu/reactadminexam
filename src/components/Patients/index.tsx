075d135acb398da7104ce8101d157765ccfb24f7
jissen-ehisamitsu <hisamitsu@jissen-cl.co.jp>
2022-06-09 22:11:14 +0900

    idは小文字でないと認識しないっぽい

--- src/components/Patients/index.tsx
+++ src/components/Patients/index.tsx
@@ -2,15 +2,19 @@ import * as React from "react";
 import {List, Datagrid, TextField, ReferenceField, SearchInput, TextInput} from "react-admin";
 
 const postFilters = [
-  <SearchInput source="q" alwaysOn />,
-  <TextInput label="Title" source="title" defaultValue="" />,
+  <SearchInput source="q" alwaysOn />
 ];
 
 export const Patients = (props:any) => (
   <List {...props} filters={postFilters}>
     <Datagrid rowClick="edit">
       <TextField source="id" />
-      <TextField source="title" />
+      <TextField source="Name" />
+      <TextField source="Birthday" />
+      <TextField source="Gender" />
+      <TextField source="Contact" />
+      <TextField source="Contact Detail" />
+      <TextField source="Insuarance Tpe" />
     </Datagrid>
   </List>
 );
